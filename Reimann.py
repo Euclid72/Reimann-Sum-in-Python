b28f4adfba4f05f7dd79082eff2bbdf6e2e50c51
İslam AYDOĞMUŞ <67843280+Euclid72@users.noreply.github.com>
2020-12-21 11:07:45 +0300

    Add files via upload

---
+++ Reimann.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 18 17:29:40 2020
+
+@author: islam
+"""
+import numpy as np
+
+# Defining Functions #
+def function(var):
+    if var == 0:
+        return 1
+    else:
+        #Try to give var as your independent variable, function should be explicid
+        return np.cos(var)
+    
+# Reimann summ method
+
+def reimann(nFrom, nTo):
+    # This is get more accurate as you chose larger the step, but I think 10x10^4 is enough
+    step = 100000
+    result = 0
+    for x in range((nTo-nFrom)*step):
+        result = result + function(x/step)/step
+    return round(result,3)
+
+#int cosx 0 to 1
+print(reimann(0,1))
